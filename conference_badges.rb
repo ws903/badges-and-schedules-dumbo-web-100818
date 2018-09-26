2234f104547b2fe6c6a78452be456d3b67e1e39c
David Sim <wsim903@gmail.com>
2018-09-26 19:38:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,6 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-def conference_badges
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  
+def conference_badges(arr)
   attendees.each do |name|
     badge_maker(name)
